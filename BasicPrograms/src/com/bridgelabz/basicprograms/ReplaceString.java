81ced832b0c2ad9c3eaf670ec83c2aa2e7a99c7c
kishorereddy0909 <kishorereddy996@gmail.com>
2019-12-31 12:37:41 +0530

    [kishore] comments were added

--- BasicPrograms/src/com/bridgelabz/basicprograms/ReplaceString.java
+++ BasicPrograms/src/com/bridgelabz/basicprograms/ReplaceString.java
@@ -6,7 +6,7 @@ public class ReplaceString {
 	public static void main(String[] args) {
 		Util util = new Util();
 		Scanner sc =  new Scanner(System.in);
-		System.out.println("Enter the username : for replacement");
+		System.out.println("Enter the username for replacement :");
 		String UserName = sc.next();
 		System.out.println(util.stringReplace(UserName));
 		
