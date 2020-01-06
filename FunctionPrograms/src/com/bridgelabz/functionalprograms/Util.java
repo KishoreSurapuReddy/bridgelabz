55dfa71c8d1ea973a7bf413e4b06f4cac6d556ba
kishorereddy0909 <kishorereddy996@gmail.com>
2020-01-06 09:45:55 +0530

    [kishorereddy] add. added Util.java to com.bridgelabz.functionalprograms

--- FunctionPrograms/src/com/bridgelabz/functionalprograms/Util.java
+++ FunctionPrograms/src/com/bridgelabz/functionalprograms/Util.java
@@ -3,32 +3,32 @@ package com.bridgelabz.functionalprograms;
 import java.util.Scanner;
 
 public class Util {
-	static Scanner sc ;
+	static Scanner sc;
 
 	/*
 	 * function for distancecalculation
 	 */
-	
+
 	/**
 	 * @param x
 	 * @param y
 	 * @return
 	 */
-	public static double distanceOf(double x,double y) {
-	    double res = Math.sqrt(Math.pow(x, 2)+Math.pow(y, 2));
+	public static double distanceOf(double x, double y) {
+		double res = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
 		return res;
 	}
 
 	/*
 	 * function for quadraticequation
 	 */
-	
+
 	/**
 	 * @param a
 	 * @param b
 	 * @param c
 	 */
-	static void quadraticEquiation(double a,double b,double c) {
+	static void quadraticEquiation(double a, double b, double c) {
 
 		double delta = b * b - 4.0 * a * c;
 
@@ -44,56 +44,56 @@ public class Util {
 		} else {
 			System.out.println("there were no roots ");
 		}
-		
+
 	}
 
 	/*
 	 * function for sumofintegers
 	 */
-	
+
 	/**
 	 * @param arr
 	 */
 	static void sumOfInteger(int[] arr) {
 		int count = 0;
-		for(int i=0;i<arr.length;i++) {
-			for(int j=i+1;j<arr.length;j++) {
-				for(int k=j+1;k<arr.length;k++) {
-					int sum = arr[i]+arr[j]+arr[k];
-					//System.out.println("sum is:"+sum);
-					if(sum==0) {
-						System.out.println(arr[i]+" "+arr[j]+" "+arr[k]);
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				for (int k = j + 1; k < arr.length; k++) {
+					int sum = arr[i] + arr[j] + arr[k];
+					// System.out.println("sum is:"+sum);
+					if (sum == 0) {
+						System.out.println(arr[i] + " " + arr[j] + " " + arr[k]);
 						count++;
 					}
 				}
 			}
 		}
-		System.out.println("no-of distinct triplets are "+count);
+		System.out.println("no-of distinct triplets are " + count);
 		return;
 	}
 
 	/*
 	 * function for twodimentionalarray
 	 */
-	
+
 	/**
 	 * @param arr
 	 */
 	static void twoDimentionArray(int[][] arr) {
 		System.out.println("elements are :");
-		for(int i=0;i<arr.length;i++) {
-			for(int j=0;j<arr.length;j++) {
-				System.out.print(" "+arr[i][j]+" ");
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				System.out.print(" " + arr[i][j] + " ");
 			}
 			System.out.println();
 		}
-		
+
 	}
 
 	/*
 	 * function for temareture of windchill
 	 */
-	
+
 	/**
 	 * @param t
 	 * @param v
@@ -101,16 +101,15 @@ public class Util {
 	 */
 	static double temparetureOf(double t, double v) {
 		double w = 0;
-		if(t<=50 && v>120 || v<3) {
+		if (t <= 50 && v > 120 || v < 3) {
 			System.out.println("enter the correct values i.e t<50 and 120>v>3");
-		}else {
-			 double a = 35.74 + (0.6215*t);
-			 double b = ((0.4275*t)-35.75)*Math.pow(v, 0.16);
-			 w = a + b;
+		} else {
+			double a = 35.74 + (0.6215 * t);
+			double b = ((0.4275 * t) - 35.75) * Math.pow(v, 0.16);
+			w = a + b;
 		}
 		return w;
-		
-	}
 
+	}
 
 }
