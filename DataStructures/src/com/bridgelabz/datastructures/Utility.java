0d192926897638f4ed3beffe8b1c41ab2b009406
kishorereddy0909 <kishorereddy996@gmail.com>
2020-01-09 10:56:24 +0530

    [kishorereddy] Add. added calander functions in Utility.java to com.bridgelabz.datastructures

--- DataStructures/src/com/bridgelabz/datastructures/Utility.java
+++ DataStructures/src/com/bridgelabz/datastructures/Utility.java
@@ -15,7 +15,7 @@ import java.util.Stack;
  */
 public class Utility<T extends Comparable<T>> {
 
-	public static Node head ,left,right; // head of list
+	public static Node head, left, right; // head of list
 	/*
 	 * initialize the size of list as zero
 	 */
@@ -54,6 +54,15 @@ public class Utility<T extends Comparable<T>> {
 
 	int rear = -1;
 	int front = -1;
+
+	/*
+	 * initialize calander array with size of 5 and 7
+	 */
+	static int[][] calander = new int[5][7];
+	/*
+	 * initialize array as monthdays
+	 */
+	static int[] monthdays = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 	/*
 	 * function for inserting element at particular position in the list
@@ -872,15 +881,15 @@ public class Utility<T extends Comparable<T>> {
 	 * function to implement enqueue of prime numbers
 	 */
 	public static void enQueuePrime() {
-		
+
 		for (int row = 0; row < rowno; row++) {
 			for (int col = 0; col < colno; col++) {
 				Node new_node = new Node(twodArray[row][col]);
-				if(right == null) {
+				if (right == null) {
 					left = right = new_node;
-				}else {
-					right.next = new_node ;
-					right  = new_node ;
+				} else {
+					right.next = new_node;
+					right = new_node;
 				}
 			}
 		}
@@ -890,7 +899,7 @@ public class Utility<T extends Comparable<T>> {
 	 * function to implement dequeue of prime numbers
 	 */
 	public static void deQueuePrime() {
-		
+
 		StringBuffer string = new StringBuffer();
 		Node node = left;
 		if (left == null) {
@@ -904,7 +913,106 @@ public class Utility<T extends Comparable<T>> {
 		}
 		string.append("]");
 		System.out.println("elements are :" + string.toString());
-		
+
+	}
+
+	/*
+	 * function to initialize the table fill table with -1
+	 */
+	public static void initCalander() {
+		row();
+	}
+
+	/*
+	 * taking row
+	 */
+	public static void row() {
+		for (int row = 0; row < calander.length; row++) {
+			col(row);
+		}
+	}
+
+	/*
+	 * taking coloumn
+	 */
+	public static void col(int row) {
+		for (int col = 0; col < calander[row].length; col++) {
+			calander[row][col] = -1;
+		}
+	}
+
+	/*
+	 * function to displaying calander table
+	 */
+	public static void displayCalTable(int month) {
+		System.out.println("       sun     mon     tue     wed      thu     fri    sat ");
+		printRow(month);
+	}
+
+	/*
+	 * printing row of table
+	 */
+	public static void printRow(int month) {
+		for (int row = 0; row < calander.length; row++) {
+			printCol(row, month);
+			System.out.println();
+		}
+	}
+
+	/*
+	 * printing coloumn of table
+	 */
+	public static void printCol(int row, int month) {
+		for (int col = 0; col < calander[row].length; col++) {
+			/*
+			 * if the value of table is monthdays < calander[row][col] < 0 print nothing
+			 */
+			if (calander[row][col] < 0 || calander[row][col] > monthdays[month - 1]) {
+				System.out.print("\t");
+			} else if (calander[row][col] > 0) {
+				System.out.print("\t" + calander[row][col] + " ");
+			}
+		}
+	}
+
+	/*
+	 * function to implement making a calander month
+	 */
+	public static void makeCalander(int day) {
+		int day1 = 1;
+		for (int col = day; col < calander[0].length; col++) {
+			calander[0][col] = day1;
+			day1++;
+		}
+		for (int row = 1; row < calander.length; row++) {
+			for (int col = 0; col < calander[row].length; col++) {
+				calander[row][col] = day1;
+				day1++;
+			}
+		}
+	}
+
+	/*
+	 * function to implement day of week
+	 */
+	static int dayOfWeek(int year, int month, int day) {
+		int y0 = year - (14 - month) / 12;
+		int x = y0 + (y0 / 4) - (y0 / 100) + (y0 / 400);
+		int m0 = month + 12 * ((14 - month) / 12) - 2;
+		int d0 = (day + x + (31 * m0) / 12) % 7;
+		return d0;
+	}
+
+	/*
+	 * function to implement display calander
+	 */
+	public static void displayCalander(int month, int year) {
+		int day = 1;
+		int d = dayOfWeek(year, month, day);
+		initCalander();
+		makeCalander(d);
+		displayCalTable(month);
+
 	}
 
 }
