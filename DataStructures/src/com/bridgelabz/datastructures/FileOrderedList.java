2f62eb17d54a1fb98c04601898f16e3e2fc0c5cf
kishorereddy0909 <kishorereddy996@gmail.com>
2020-01-11 18:38:41 +0530

    [kishorereddy] Refractor. code modified in FileOrderedList.java in com.bridgelabz.datastructures

--- DataStructures/src/com/bridgelabz/datastructures/FileOrderedList.java
+++ DataStructures/src/com/bridgelabz/datastructures/FileOrderedList.java
@@ -2,13 +2,12 @@
 /*Purpose:Read the data from the user and added into the list in ascending order
  * and take a another user input if it is found remove from the list, if it 
  * was not found added to list at particular position
-*/
-
-/**
+ * 
  * @author kishorereddy
- *
  */
 package com.bridgelabz.datastructures;
+
+import com.bridgelabz.util.OrderedList;
 
 public class FileOrderedList {
 
@@ -18,49 +17,42 @@ public class FileOrderedList {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		/*
-		 * creating the instance of utility class to access the function in it
-		 */
-		Utility<Integer> util = new Utility<>();
+
+		OrderedList<Integer> list = new OrderedList<Integer>();
 
 		int number = 0;
 		try {
 			System.out.println("enter the number how many numbers want to add :");
-			number = util.scanner.nextInt();
+			number = list.scanner.nextInt();
 		} catch (Exception e) {
 			System.out.println("enter in digits only...");
 		}
-		// asking the user to assign the size of list
 
 		System.out.println("enter number :");
 
 		for (int index = 0; index < number; index++) {
 			System.out.println("enter " + index + " no");
-			/*
-			 * taking the user inputs from the keyboard and adding the elements through this
-			 * function
-			 */
+			// taking the user inputs from the keyboard and adding the elements through this
+			// function
 			try {
-				util.addOrderedElement(util.scanner.nextInt());
+				list.addOrderedElement(list.scanner.nextInt());
 			} catch (Exception e) {
 				System.out.println("enter only int type of data...");
 			}
 		}
 		// printing the list
-		util.printList();
+		list.printList();
 		// asking user to enter the element to search in the list
 		int searchnum = 0;
 		try {
 			System.out.println("enter number to search :");
-			searchnum = util.scanner.nextInt();
+			searchnum = list.scanner.nextInt();
 		} catch (Exception e) {
 			System.out.println("enter only int type of data...");
 		}
-		/*
-		 * calling function for adding and removing elements from list
-		 */
-		Utility.addRemove(searchnum, util);
-		util.scanner.close();
+		// calling function for adding and removing elements from list
+		OrderedList.addRemove(searchnum, list);
+		list.scanner.close();
 
 	}
 }
