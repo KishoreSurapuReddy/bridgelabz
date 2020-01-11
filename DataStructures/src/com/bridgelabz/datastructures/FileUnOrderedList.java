47a30ab2bbc5410416284de5682cf7b2dd36de07
kishorereddy0909 <kishorereddy996@gmail.com>
2020-01-11 18:40:25 +0530

    [kishorereddy] Refractor. code modified in FileUnOrderedList.java in com.bridgelabz.datastructures

--- DataStructures/src/com/bridgelabz/datastructures/FileUnOrderedList.java
+++ DataStructures/src/com/bridgelabz/datastructures/FileUnOrderedList.java
@@ -4,11 +4,8 @@
 * if u find any element remove from the list and if u not find any element just
 * add that element into the list and finally save all the list elements into file
 * 
+* @author kishorereddy
 */
-/**
- * @author kishorereddy
- *
- */
 package com.bridgelabz.datastructures;
 
 import java.io.File;
@@ -17,13 +14,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
+import com.bridgelabz.util.UnOrderedList;
+
 public class FileUnOrderedList {
 	/*
 	 * main function to test the class
 	 */
 	public static void main(String[] args) {
 
-		Utility<String> util = new Utility<String>();
+		UnOrderedList<String> list = new UnOrderedList<String>();
 
 		try {
 			File file = new File("/home/bridgelabsz/Desktop/unordered.txt");
@@ -32,49 +31,49 @@ public class FileUnOrderedList {
 
 			while (scannerfile.hasNext()) {
 
-				util.addElement(scannerfile.next());
+				list.addElement(scannerfile.next());
 				// adding elements to the list from file
 			}
 			// printing the list
 
-			util.printList();
+			list.printList();
 			FileWriter filewriter = new FileWriter(file);
 
 			// fileWriter to write the content into the file
-			System.out.println("the size of list :" + Utility.sizeOfList());
+			System.out.println("the size of list :" + list.sizeOfList());
 
 			System.out.println("enter the word to search :");
 
-			String find = util.scanner.nextLine();
+			String find = list.scanner.nextLine();
 			// taking the user input element for searching in list
-			if (util.searchElement(find)) {
+			if (list.searchElement(find)) {
 
 				// if u find the element it will return
-				util.removeElement(find);
+				list.removeElement(find);
 				// it will remove the element from the list
 				int n = 0;
-				while (n < Utility.sizeOfList()) {
+				while (n < list.sizeOfList()) {
 					// it will fetch the element from the list and write into file
 					// it will clear the stream by flush
-					filewriter.write(util.pop(0) + " ");
+					filewriter.write(list.pop(0) + " ");
 					filewriter.flush();
 					n++;
 				}
 				System.out.println("item was founded and deleted ");
 			} else {
 
-				util.addElement(find);
+				list.addElement(find);
 				// if the element not found in the list it will add to
 				// the list
 				int n = 0;
-				while (n < Utility.sizeOfList()) {
-					filewriter.write(util.pop(0) + " ");
+				while (n < list.sizeOfList()) {
+					filewriter.write(list.pop(0) + " ");
 					filewriter.flush();
 					n++;
 				}
 				System.out.println("item was added ");
 			}
-			util.scanner.close();
+			list.scanner.close();
 			scannerfile.close();
 			filewriter.close();
 
