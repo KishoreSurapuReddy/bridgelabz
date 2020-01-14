b86ba97207cac6430a8147e0f05f4d3893ba45ae
kishorereddy0909 <kishorereddy996@gmail.com>
2020-01-14 19:37:15 +0530

    [kishorereddy0909] Refractor. code modified in AddressBook in com.bridgelabz.objectorinted

--- ObjectOrinted/src/com/bridgelabz/addressbook/AddressDAO.java
+++ ObjectOrinted/src/com/bridgelabz/addressbook/AddressDAO.java
@@ -4,11 +4,10 @@ public interface AddressDAO {
 	public void addPerson();
 	public void deletePerson(String firstName);
 	public void editPerson(String firstName);
-	public AddressBook sortEntriesByName(String array);
 	public AddressBook sortEntriesByZipCode(int zipCode);
 	public AddressBook[] printEntries();
 	public void createNewAddressBook();
-	public void openExistingAddressBook(AddressBook[] addressbook);
+	public void openExistingAddressBook(AddressBook addressbook);
 	public void saveAddressBook();
 	public void saveAsAddressBook();
 
