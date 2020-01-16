ad4c78bfc23f71020d148525d7372b97296fb18d
kishorereddy0909 <kishorereddy996@gmail.com>
2020-01-16 11:35:23 +0530

    [kishorereddy] Add. added AddressBookDAOImpl.java to com.bridgelabz.addressbook

--- ObjectOrinted/src/com/bridgelabz/addressbook/AddressBookDAOIpml.java
+++ ObjectOrinted/src/com/bridgelabz/addressbook/AddressBookDAOIpml.java
@@ -1,191 +1,178 @@
 package com.bridgelabz.addressbook;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.Scanner;
 
-public  class AddressBookDAOIpml implements AddressDAO {
-	static AddressBook[] array = new AddressBook[2];
-	AddressBook[] collection = new AddressBook[2];
-	static AddressBook[] temp = new AddressBook[2];
-	//AddressBook[] newaddress = new AddressBook[1];
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class AddressBookDAOIpml implements AddressDAO {
 	Scanner scanner = new Scanner(System.in);
-	AddressBook addressbook ;
 
+	/*
+	 * function to implement adding person
+	 */
 	@Override
-	public void addPerson() {
-		array = new AddressBook[1];
-		for (int position = 0; position < array.length; position++) {
-			String firstName = null;
-			String lasttName = null;
-			String address = null;
-			String city = null;
-			String state = null;
-			int zipCode = 0;
-			long phoneNumber = 0;
-			try {
-				System.out.println("Enter the person details: ");
-				System.out.println("Enter FirstName :");
-				firstName = scanner.next();
-				System.out.println("Enter LastName :");
-				lasttName = scanner.next();
-				System.out.println("Enter Address :");
-				address = scanner.next();
-				System.out.println("Enter City :");
-				city = scanner.next();
-				System.out.println("Enter State :");
-				state = scanner.next();
-				System.out.println("Enter ZipCode :");
-				zipCode = scanner.nextInt();
-				System.out.println("Enter PhoneNumber :");
-				phoneNumber = scanner.nextLong();
-			} catch (Exception e) {
-				System.out.println("enter correct data");
+	public void addPerson() throws IOException, ParseException {
+		AddressBook address = new AddressBook();
+		System.out.println("enter the first name :");
+		address.setFirstName(scanner.next());
+		System.out.println("enter last name :");
+		address.setLastName(scanner.next());
+		System.out.println("enter address :");
+		address.setAddress(scanner.next());
+		System.out.println("enter city :");
+		address.setCity(scanner.next());
+		System.out.println("enter state :");
+		address.setState(scanner.next());
+		System.out.println("enter zipcode :");
+		address.setZipCode(scanner.nextInt());
+		System.out.println("enter phonenumber :");
+		address.setPhoneNumber(scanner.nextLong());
+
+		writeDetails(address.getFirstName(), address.getLastName(), address.getAddress(), address.getCity(),
+				address.getState(), address.getZipCode(), address.getPhoneNumber());
+
+	}
+
+	/*
+	 * function to implement delete person
+	 */
+	@Override
+	public void deletePerson(String firstName) throws IOException, ParseException {
+		JSONParser parser = new JSONParser();
+		FileReader file = new FileReader("/home/bridgelabsz/Desktop/address.json");
+		Object object = parser.parse(file);
+		JSONObject jsonobject = (JSONObject) object;
+		jsonobject.get(firstName);
+		jsonobject.remove(firstName);
+		FileWriter filewriter = new FileWriter("/home/bridgelabsz/Desktop/address.json");
+		filewriter.write(jsonobject.toJSONString());
+		filewriter.close();
+	}
+
+	/*
+	 * function to implement update person
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public void editPerson(String firstName) throws IOException, ParseException {
+		JSONParser parser = new JSONParser();
+		try {
+			FileReader file = new FileReader("/home/bridgelabsz/Desktop/address.json");
+			Object object = parser.parse(file);
+			JSONObject jsonobject = (JSONObject) object;
+			if (jsonobject.get(firstName) != null) {
+				JSONArray jsonarray = (JSONArray) jsonobject.get(firstName);
+				JSONObject json = (JSONObject) jsonarray.get(0);
+				json.forEach((key, value) -> {
+					if (!key.equals(firstName)) {
+						System.out.println(key + "\t\t" + value);
+						System.out.println("enter 1 to edit the details or press any key");
+						int num = scanner.nextInt();
+						if (num == 1) {
+							System.out.println("enter the details :");
+							if (key.equals("zipCode") || key.equals("phoneNumber")) {
+								json.put(key, scanner.nextInt());
+							} else {
+								json.put(key, scanner.next());
+							}
+						}
+					}
+				});
+				System.out.println("after change :");
+				json.forEach((key, value) -> System.out.println(key + "\t\t" + value));
+				FileWriter filewriter = new FileWriter("/home/bridgelabsz/Desktop/address.json");
+				filewriter.write(json.toJSONString());
+				filewriter.close();
 			}
-			addressbook = new AddressBook(firstName,lasttName,address,city,state,zipCode,phoneNumber);
-			array[position] = addressbook;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		System.out.println("successfully record added..");
+
 	}
 
+	/*
+	 * function to implement save addressbook
+	 */
 	@Override
-	public void deletePerson(String firstName) {
-		int index = 0;
-		temp =  new AddressBook[2];
-		for(AddressBook item : array) {
-			if(item.getFirstName().equals(firstName)) {
-				
-			}else {
-				temp[index] = item;
-			}
-			index++;
+	public void saveAddressBook() throws IOException, ParseException {
+		JSONParser parser = new JSONParser();
+		try {
+			FileReader file = new FileReader("/home/bridgelabsz/Desktop/address.json");
+			Object object = parser.parse(file);
+			JSONObject jsonobject = (JSONObject) object;
+			System.out.println("enter the name of address book :");
+			String name = scanner.next();
+			FileWriter filewriter = new FileWriter("/home/bridgelabsz/Desktop/" + name + ".json");
+			filewriter.write(jsonobject.toJSONString());
+			filewriter.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		array = new AddressBook[2];
-		for(int position =0 ; position < array.length ; position++) {
-			array[position] = temp[position];
-		}
-		System.out.println("successfully address is deleted..");
-		
-	}
-	
 
+	}
+
+	/*
+	 * function to implement write person 
+	 */
+	@SuppressWarnings("unchecked")
 	@Override
-	public void editPerson(String firstName) {
-		int index = 0;
-		temp = new AddressBook[2];
-		for(AddressBook item : array) {
-			if(item.getFirstName().equals(firstName)) {
-				int choice = 0;
-				try {
-					System.out.println("select the choice to update 1.address 2.city 3.state 4.zipcode 5.phonenumber");
-					choice = scanner.nextInt();
-				} catch (Exception e) {
-					System.out.println("enter digits only..");
-				}
-				switch(choice) {
-				case 1:
-					System.out.println("enter address to update");
-					item.setAddress(scanner.next());
-					break;
-				case 2:
-					System.out.println("enter city to update ");
-					item.setCity(scanner.next());
-					break;
-				case 3:
-					System.out.println("enter state to update ");
-					item.setState(scanner.next());
-					break;
-				case 4:
-					System.out.println("enter zipcode to update ");
-					item.setZipCode(scanner.nextInt());
-					break;
-				case 5:
-					System.out.println("enter phonenumber to update ");
-					item.setPhoneNumber(scanner.nextLong());
-					break;
-				default :
-					System.out.println("select only range between 1-5");
-					break;
-				}
-				temp[index] = item;
-			}else {
-				temp[index] = item ;
-			}
-			index++;
-		}
-		array = new AddressBook[2];
-		for(int position = 0 ; position < temp.length ; position++) {
-			array[position] = temp[position];
-		}
-		System.out.println("successfully updated ");
+	public void writeDetails(String firstName, String lastName, String address, String city, String state, int zipCode,
+			long phoneNumber) throws IOException, ParseException {
+		JSONParser parser = new JSONParser();
+		FileReader file = new FileReader("/home/bridgelabsz/Desktop/address.json");
+		Object object = parser.parse(file);
+		JSONObject jsonobject = (JSONObject) object;
+		JSONArray jsonarray = new JSONArray();
+		JSONObject jsonobj = new JSONObject();
+		jsonobj.put("firstName", firstName);
+		jsonobj.put("lastName", lastName);
+		jsonobj.put("address", address);
+		jsonobj.put("city", city);
+		jsonobj.put("state", state);
+		jsonobj.put("zipCode", zipCode);
+		jsonobj.put("phoneNumber", phoneNumber);
+		jsonarray.add(jsonobj);
+		System.out.println(jsonarray);
+		jsonobject.put(firstName, jsonarray);
+		FileWriter filewriter = new FileWriter("/home/bridgelabsz/Desktop/address.json");
+		filewriter.write(jsonobject.toJSONString());
+		filewriter.close();
 	}
 
-	
+	/*
+	 * function to implement search person
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
-	public AddressBook sortEntriesByZipCode(int zipCode) {
-		return addressbook;
-		
-	}
-
-	@Override
-	public AddressBook[] printEntries() {
-		return array;
-		
-	}
-
-	@Override
-	public void createNewAddressBook() {
-		for(int index = 0 ; index < collection.length ; index ++) {
-			collection = array;
-		}
-		addPerson();
-		collection = array;
-	}
-
-	@Override
-	public void openExistingAddressBook(AddressBook addressbook) {
-		for(int position = 0 ; position < collection.length ; position++) {
-			AddressBook selectedbook = collection[position];
-			if(addressbook.equals(selectedbook)) {
-				System.out.println(addressbook.getFirstName()+"   "
-						+addressbook.getLastName()+"   "+
-						addressbook.getAddress()+"    "+
-						addressbook.getCity()+"   "+
-						addressbook.getState()+"   "+
-						addressbook.getZipCode()+"    "+
-						addressbook.getPhoneNumber());
-			}
-		}
-		
-	}
-
-	@Override
-	public void saveAddressBook() {
-		
-	}
-
-	@Override
-	public void saveAsAddressBook() {
-		System.out.println("enter the name of address book");
-		
-	}
-	public void noofAddressBooks(AddressBook newaddress2) {
-		AddressBook[] addressbooks = new AddressBook[5];
-		for(int position = 0 ; position < addressbooks.length ; position++) {
-			addressbooks[position] = newaddress2;
-		}
-	}
-
-	//@Override
-	/*public <T extends Comparable<T>>T[] sortEntriesByName(T[] array) {
-		for(int count = 1 ; count < array.length ; count++) {
-			for(int index = count ; index > 0 ;index--) {
-				if(array[index-1].compareTo(array[index]) > 0) {
-					String temp = (String) array[index];
-					array[index] = array[index-1];
-					array[index-1] = (T) temp;
+	public void searchEntries(String firstname) throws IOException, ParseException {
+		JSONParser parser = new JSONParser();
+		try {
+			FileReader file = new FileReader("/home/bridgelabsz/Desktop/address.json");
+			Object object = parser.parse(file);
+			JSONObject jsonobject = (JSONObject) object;
+			JSONArray jsonarray = (JSONArray) jsonobject.get(firstname);
+			if (jsonarray != null) {
+				Iterator iterator = jsonarray.iterator();
+				while (iterator.hasNext()) {
+					JSONObject json = (JSONObject) iterator.next();
+					json.forEach((key, value) -> System.out.println(key + "\t\t" + value));
 				}
 			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-		return array;
-	}*/
+
+	}
+
 }
