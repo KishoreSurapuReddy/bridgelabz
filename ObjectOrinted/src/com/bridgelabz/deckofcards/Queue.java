a0ac409de02797c2588eb9b97c24e2a12a8a7417
kishorereddy0909 <kishorereddy996@gmail.com>
2020-01-16 11:53:26 +0530

    [kishorereddy]Refractor. code modified in Queue.java in com.bridgelabz.deckofcards

--- ObjectOrinted/src/com/bridgelabz/deckofcards/Queue.java
+++ ObjectOrinted/src/com/bridgelabz/deckofcards/Queue.java
@@ -1,23 +1,33 @@
 package com.bridgelabz.deckofcards;
+
 import com.bridgelabz.deckofcards.Node;
+
 public class Queue {
-	 Node head;
-	public  void enQueue(String item) {
+	Node head;
+
+	/*
+	 * function to implement enqueue in Queue
+	 */
+	public void enQueue(String item) {
 		Node newNode = new Node(item);
-		if(head == null) {
+		if (head == null) {
 			head = newNode;
-		}else {
+		} else {
 			newNode.next = head;
 			head = newNode;
 		}
 	}
+
+	/*
+	 * function to implement dequeue in queue
+	 */
 	public void deQueue() {
-		Node node = head ;
-		if(node == null) {
+		Node node = head;
+		if (node == null) {
 			System.out.println("queue is empty..");
-		}else {
-			while(node != null) {
-				System.out.println("sorted deck :"+node.data);
+		} else {
+			while (node != null) {
+				System.out.println("sorted deck :" + node.data);
 				node = node.next;
 			}
 		}
