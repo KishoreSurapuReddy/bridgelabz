74fbeb33f11ef30ef860774bc0358a39e7ec65d0
kishorereddy0909 <kishorereddy996@gmail.com>
2020-01-16 11:33:29 +0530

    [kishorereddy] Add. added AddressBook.java to com.bridgelabz.addressbook

--- ObjectOrinted/src/com/bridgelabz/addressbook/AddressBook.java
+++ ObjectOrinted/src/com/bridgelabz/addressbook/AddressBook.java
@@ -8,10 +8,11 @@ public class AddressBook {
 	private String state;
 	private int zipCode;
 	private long phoneNumber;
-	
+
 	public AddressBook() {
-		
+
 	}
+
 	public AddressBook(String firstName, String lastName, String address, String city, String state, int zipCode,
 			long phoneNumber) {
 		this.firstName = firstName;
@@ -22,51 +23,66 @@ public class AddressBook {
 		this.zipCode = zipCode;
 		this.phoneNumber = phoneNumber;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
+
 	public String getState() {
 		return state;
 	}
+
 	public void setState(String state) {
 		this.state = state;
 	}
+
 	public int getZipCode() {
 		return zipCode;
 	}
+
 	public void setZipCode(int zipCode) {
 		this.zipCode = zipCode;
 	}
+
 	public long getPhoneNumber() {
 		return phoneNumber;
 	}
+
 	public void setPhoneNumber(long phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
-	
+
 	public String toString() {
-		return getFirstName()+" "+getLastName()+" "+getAddress()+" "+getCity()+" "+getState()+" "+getZipCode()+" "+getPhoneNumber();
+		return getFirstName() + " " + getLastName() + " " + getAddress() + " " + getCity() + " " + getState() + " "
+				+ getZipCode() + " " + getPhoneNumber();
 	}
 
 }
