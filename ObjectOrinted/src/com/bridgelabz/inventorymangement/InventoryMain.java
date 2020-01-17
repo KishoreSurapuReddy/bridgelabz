2c02cefefac18ee99d679c92cad5fb967f91e983
kishorereddy0909 <kishorereddy996@gmail.com>
2020-01-17 19:29:49 +0530

    [kishorereddy] Refractor. code modified in InventoryMain.java in com.bridgelabz.inventorymanagement

--- ObjectOrinted/src/com/bridgelabz/inventorymangement/InventoryMain.java
+++ ObjectOrinted/src/com/bridgelabz/inventorymangement/InventoryMain.java
@@ -1,14 +1,17 @@
+/*Purpose:Inventory management is used to add the products into the file and 
+ * we can also perform operation like calculating the total value of product
+ * @author kishorereddy
+*/
 package com.bridgelabz.inventorymangement;
 
 import java.io.IOException;
 
 import org.json.simple.parser.ParseException;
 
-
 public class InventoryMain {
-	
+
 	InventoryDAOImpl inventory = new InventoryDAOImpl();
-	
+
 	public void menu() throws IOException {
 		System.out.println("--------------------");
 		System.out.println("enter the option.... ");
@@ -23,59 +26,69 @@ public class InventoryMain {
 			menu();
 			break;
 		case 2:
-			System.out.println("select the calculation choice ");
-			System.out.println("1. rice calulation ");
-			System.out.println("2. wheat calculation ");
-			System.out.println("3. pulses calculation ");
-			System.out.println("4. all ");
-			int choice = inventory.scanner.nextInt();
-			switch (choice) {
-			case 1:
-				System.out.println("calulation for rice");
-				try {
-					System.out.println(inventory.readInventory("Rice"));
-				} catch (ParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				break;
-			case 2:
-				System.out.println("calculation for wheat ");
-				try {
-					System.out.println(inventory.readInventory("wheat"));
-				} catch (ParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-				break;
-			case 3:
-				System.out.println("calculation for pulses ");
-				try {
-					System.out.println(inventory.readInventory("pulses"));
-				} catch (ParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-				break;
-			case 4:
-				System.out.println("calculation for all ");
-				try {
-					System.out.println(inventory.readInventory("All"));
-				} catch (ParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				break;
-
-			default:
-				System.out.println("wrong choice");
-				break;
-			}
+			case2();
+			menu();
+			break;
+		case 3:
+			System.exit(0);
 			break;
 		default:
-			System.out.println("enter choice 1-4 only");
+			System.out.println("enter choice 1-3 only");
+			break;
+		}
+
+	}
+
+	public void case2() throws IOException {
+
+		System.out.println("select the calculation choice ");
+		System.out.println("1. rice calulation ");
+		System.out.println("2. wheat calculation ");
+		System.out.println("3. pulses calculation ");
+		System.out.println("4. all ");
+		int choice = inventory.scanner.nextInt();
+		switch (choice) {
+		case 1:
+			System.out.println("calulation for rice");
+			try {
+				System.out.println(inventory.readInventory("Rice"));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+		case 2:
+			System.out.println("calculation for wheat ");
+			try {
+				System.out.println(inventory.readInventory("wheat"));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			break;
+		case 3:
+			System.out.println("calculation for pulses ");
+			try {
+				System.out.println(inventory.readInventory("pulses"));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			break;
+		case 4:
+			System.out.println("calculation for all ");
+			try {
+				System.out.println(inventory.readInventory("All"));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+
+		default:
+			System.out.println("wrong choice");
 			break;
 		}
 
