a73989d2ed6c0349043a1af9fb25ce77571b5faa
kishorereddy0909 <kishorereddy996@gmail.com>
2020-01-17 19:31:19 +0530

    [kishorereddy] Refractor. code modified in StockManagement.java in com.bridgelabz.stockmanagement

--- ObjectOrinted/src/com/bridgelabz/stockmanagement/StockManagement.java
+++ ObjectOrinted/src/com/bridgelabz/stockmanagement/StockManagement.java
@@ -1,42 +1,43 @@
 package com.bridgelabz.stockmanagement;
 
 public class StockManagement {
-	
+
 	String stockName;
 	int numberOfShares;
 	double sharePrice;
-	
+
 	public StockManagement() {
-		
+
 	}
-	
+
 	public StockManagement(String stockName, int numberOfShares, double sharePrice) {
 		this.stockName = stockName;
 		this.numberOfShares = numberOfShares;
 		this.sharePrice = sharePrice;
 	}
-	
+
 	public String getStockName() {
 		return stockName;
 	}
+
 	public void setStockName(String stockName) {
 		this.stockName = stockName;
 	}
+
 	public int getNumberOfShares() {
 		return numberOfShares;
 	}
+
 	public void setNumberOfShares(int numberOfShares) {
 		this.numberOfShares = numberOfShares;
 	}
+
 	public double getSharePrice() {
 		return sharePrice;
 	}
+
 	public void setSharePrice(double sharePrice) {
 		this.sharePrice = sharePrice;
 	}
-	
-	
-	
-	
 
 }
