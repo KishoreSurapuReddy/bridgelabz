a999b439454f934787460c5290a863886509b573
kishorereddy0909 <kishorereddy996@gmail.com>
2020-01-17 19:32:48 +0530

    [kishorereddy] Refractor . code modified in StockManagementDAO.java in com.bridgelabz.stockmanagemnet

--- ObjectOrinted/src/com/bridgelabz/stockmanagement/StockManagementDAO.java
+++ ObjectOrinted/src/com/bridgelabz/stockmanagement/StockManagementDAO.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.stockmanagement;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.json.simple.parser.ParseException;
@@ -12,5 +13,13 @@ public interface StockManagementDAO {
 	public void updateStock(String stockName) throws IOException, ParseException;
 	public void deleteStock(String stockName) throws IOException, ParseException;
 	public void writeIntoFile(String stockName,int numberOfShares , double sharePrice) throws IOException, ParseException;
+	
+	public void stockAccount() throws IOException, ParseException;
+	public int[] valueOfAccount(String customerName) throws IOException, ParseException;
+	public void buyShare(int amount , String stockName) throws IOException, ParseException;
+	public void sellShare(int amount , String stockName);
+	public void saveFile(String fileName) throws FileNotFoundException, IOException, ParseException;
+	public void printReport(String customerName) throws IOException, ParseException;
+	public void write(String customerName , int customerId,String[] stockName ,int[] numberOfshares ,double[] shareValue ) throws IOException, ParseException;
 
 }
