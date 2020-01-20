3d55d1bf0e4d2a220f2c2b8edce5b43d12ddc15c
kishorereddy0909 <kishorereddy996@gmail.com>
2020-01-20 18:09:34 +0530

    [kishorereddy] Refractor. added comments to AddressDAO.java in com.bridgelabz.address

--- ObjectOrinted/src/com/bridgelabz/addressbook/AddressDAO.java
+++ ObjectOrinted/src/com/bridgelabz/addressbook/AddressDAO.java
@@ -1,3 +1,9 @@
+/*purpose:it will have abstract methods
+ * @author kishorereddy
+ * @version 1.0
+ * @since 14/01/2020
+ * @file AddressDAO.java
+ */
 package com.bridgelabz.addressbook;
 
 import java.io.FileNotFoundException;
@@ -7,7 +13,7 @@ import org.json.simple.parser.ParseException;
 
 public interface AddressDAO {
 	//abstract methods
-	public void addPerson() throws IOException, ParseException;
+	public void addPerson(Person person ) throws IOException, ParseException;
 	public void deletePerson(String firstName) throws FileNotFoundException, IOException, ParseException;
 	public void editPerson(String firstName) throws IOException, ParseException;
 	public void writeDetails(String firstName , String lastName , String address , String city , String state , int zipCode ,long phoneNumber ) throws FileNotFoundException, IOException, ParseException;
