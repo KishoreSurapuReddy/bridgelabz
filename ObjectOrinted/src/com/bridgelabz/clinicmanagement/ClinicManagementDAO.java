40b8aff130017d77394a95d5ae3f5e898a409cf1
kishorereddy0909 <kishorereddy996@gmail.com>
2020-01-20 18:13:35 +0530

    [kishorereddy] Refractor. added comments to ClinicmanagementDAO.java in com.bridgelabz.clinicmanagement

--- ObjectOrinted/src/com/bridgelabz/clinicmanagement/ClinicManagementDAO.java
+++ ObjectOrinted/src/com/bridgelabz/clinicmanagement/ClinicManagementDAO.java
@@ -1,17 +1,23 @@
+/*purpose:ClinicManagementDAO have abstract methods
+ * @author kishorereddy
+ * @version 1.0
+ * @since 15/01/2020
+ * @file ClinicManagementDAO.java
+ */
 package com.bridgelabz.clinicmanagement;
 
 import java.util.List;
-
+//interface class
 public interface ClinicManagementDAO {
-	
-	public void addDoctor();
-	public void addPatient();
+	//abstract methods
+	public void addDoctor(Doctor doctor);
+	public void addPatient(Patient patient);
 	public void takeAppointment(String doctorname);
-	public void updateDoctor(String doctorName);
-	public void updatePatient(String patientName);
-	public Doctor viewDoctor(String doctorName ,int doctorId , String specilazition , String availability);
+	public void updateDoctor(int doctorId);
+	public void updatePatient(int patientId);
+	public Doctor viewDoctor(int doctorId);
 	public List<Doctor> viewAllDoctors();
 	public List<Patient> viewAllPatients();
-    public Patient viewPatient(String patientName , int patientId , long phoneNumber );
+    public Patient viewPatient( int patientId );
 
 }
