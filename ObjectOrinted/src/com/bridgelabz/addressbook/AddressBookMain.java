4101f9bc44bcfa3a1cc17064aedcc325af345559
kishorereddy0909 <kishorereddy996@gmail.com>
2020-01-20 18:07:47 +0530

    [kishorereddy] Refractor. taking person details and added try catch block to check input in AddressBookMain.java in com.bridgelabz.addressbook

--- ObjectOrinted/src/com/bridgelabz/addressbook/AddressBookMain.java
+++ ObjectOrinted/src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -1,7 +1,10 @@
 /*Purpose: create address and add person to it and also update the person and 
  * we can also delete the person from address book 
  * @author kishorereddy
-*/
+ * @version 1.0
+ * @since 14/01/2020
+ * @file AddressBookMain.java
+ */
 package com.bridgelabz.addressbook;
 
 import java.io.IOException;
@@ -10,7 +13,7 @@ import org.json.simple.parser.ParseException;
 
 public class AddressBookMain {
 	AddressBookDAOIpml address = new AddressBookDAOIpml();
-	AddressBook select1;
+	Person select1;
 	public void menu() throws IOException, ParseException {
 		System.out.println("------------------------");
 		System.out.println("       ADDRESSBOOK      ");
@@ -27,28 +30,70 @@ public class AddressBookMain {
 		} catch (Exception e) {
 			System.out.println("enter digits only..");
 		}
-		switch(choice) {
+		switch (choice) {
 		case 1:
-			try {
-				address.addPerson();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			int loop = 1;
+			while (loop == 1) {
+				Person person = new Person();
+				try {
+					System.out.println("enter the first name :");
+					person.setFirstName(address.scanner.next());
+					System.out.println("enter last name :");
+					person.setLastName(address.scanner.next());
+					System.out.println("enter address :");
+					person.setAddress(address.scanner.next());
+					System.out.println("enter city :");
+					person.setCity(address.scanner.next());
+					System.out.println("enter state :");
+					person.setState(address.scanner.next());
+				} catch (Exception e1) {
+					System.out.println("enter string type of data..");
+				}
+				try {
+					System.out.println("enter zipcode :");
+					person.setZipCode(address.scanner.nextInt());
+					System.out.println("enter phonenumber :");
+					person.setPhoneNumber(address.scanner.nextLong());
+				} catch (Exception e1) {
+					System.out.println("enter intger type of data... ");
+				}
+				try {
+					address.addPerson(person);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				try {
+					System.out.println("if you want to add another person details press 1 or press any key");
+					loop = address.scanner.nextInt();
+				} catch (Exception e) {
+					System.out.println("enter digits only...");
+				}
 			}
 			menu();
 			break;
 		case 2:
-			System.out.println("enter the first name to which record to modify:");
-			String name = address.scanner.next();
+			String name = null;
+			try {
+				System.out.println("enter the first name to which record to modify:");
+				name = address.scanner.next();
+			} catch (Exception e) {
+				System.out.println("enter string type of data...");
+			}
 			address.editPerson(name);
 			menu();
 			break;
 		case 3:
-			System.out.println("enter the first name to which record to delete ");
-			String deletename = address.scanner.next();
+			String deletename = null;
+			try {
+				System.out.println("enter the first name to which record to delete ");
+				deletename = address.scanner.next();
+			} catch (Exception e) {
+				System.out.println("enter string type of data..");
+			}
 			address.deletePerson(deletename);
 			menu();
 			break;
@@ -57,9 +102,13 @@ public class AddressBookMain {
 			menu();
 			break;
 		case 5:
-			System.out.println("enter the first name :");
-			String firstname = address.scanner.next();
-			address.searchEntries(firstname);
+			try {
+				System.out.println("enter the first name :");
+				String firstname = address.scanner.next();
+				address.searchEntries(firstname);
+			} catch (Exception e) {
+				System.out.println("enter String type of data..");
+			}
 			menu();
 			break;
 		case 6:
@@ -72,12 +121,10 @@ public class AddressBookMain {
 		}
 	}
 
-
 	public static void main(String[] args) throws IOException, ParseException {
-		
+
 		AddressBookMain address = new AddressBookMain();
 		address.menu();
 	}
-
 
 }
