fba255a20602b685f27687bc1922bf87dd0c4ab2
kishorereddy0909 <kishorereddy996@gmail.com>
2020-01-20 18:04:28 +0530

    [kishorereddy] Refractor. gives person as parameter to addPerson and removed readFile and writeFile methods in AddressBookDAOImpl.java in com.bridgelabz.addressbook

--- ObjectOrinted/src/com/bridgelabz/addressbook/AddressBookDAOIpml.java
+++ ObjectOrinted/src/com/bridgelabz/addressbook/AddressBookDAOIpml.java
@@ -1,10 +1,15 @@
+/*purpose:it will implement abstract methods 
+ * @author kishorereddy
+ * @version 1.0
+ * @since 14/01/2020
+ * @file AddressBookDAOImpl.java
+ */
 package com.bridgelabz.addressbook;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.Scanner;
 
 import org.json.simple.JSONArray;
@@ -12,70 +17,55 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import com.bridgelabz.filesystem.FileSystem;
+
 public class AddressBookDAOIpml implements AddressDAO {
 	Scanner scanner = new Scanner(System.in);
-
+	String addressFile = "/home/bridgelabsz/Desktop/address.json";
+	FileSystem file = new FileSystem();
 	/*
 	 * function to implement adding person
 	 */
 	@Override
-	public void addPerson() throws IOException, ParseException {
-		AddressBook address = new AddressBook();
-		System.out.println("enter the first name :");
-		address.setFirstName(scanner.next());
-		System.out.println("enter last name :");
-		address.setLastName(scanner.next());
-		System.out.println("enter address :");
-		address.setAddress(scanner.next());
-		System.out.println("enter city :");
-		address.setCity(scanner.next());
-		System.out.println("enter state :");
-		address.setState(scanner.next());
-		System.out.println("enter zipcode :");
-		address.setZipCode(scanner.nextInt());
-		System.out.println("enter phonenumber :");
-		address.setPhoneNumber(scanner.nextLong());
+	public void addPerson(Person person) throws IOException, ParseException {
 
-		writeDetails(address.getFirstName(), address.getLastName(), address.getAddress(), address.getCity(),
-				address.getState(), address.getZipCode(), address.getPhoneNumber());
+		writeDetails(person.getFirstName(), person.getLastName(), person.getAddress(), person.getCity(),
+				person.getState(), person.getZipCode(), person.getPhoneNumber());
 
 	}
-
 	/*
 	 * function to implement delete person
 	 */
 	@Override
 	public void deletePerson(String firstName) throws IOException, ParseException {
-		JSONParser parser = new JSONParser();
-		FileReader file = new FileReader("/home/bridgelabsz/Desktop/address.json");
-		Object object = parser.parse(file);
-		JSONObject jsonobject = (JSONObject) object;
-		jsonobject.get(firstName);
+		String fileReader = addressFile;
+		JSONObject jsonobject = file.readFile(fileReader);
 		jsonobject.remove(firstName);
-		FileWriter filewriter = new FileWriter("/home/bridgelabsz/Desktop/address.json");
-		filewriter.write(jsonobject.toJSONString());
-		filewriter.close();
+		String filewriter = addressFile;
+		file.writeFile(filewriter, jsonobject);
 	}
-
 	/*
 	 * function to implement update person
 	 */
 	@SuppressWarnings("unchecked")
 	@Override
 	public void editPerson(String firstName) throws IOException, ParseException {
-		JSONParser parser = new JSONParser();
 		try {
-			FileReader file = new FileReader("/home/bridgelabsz/Desktop/address.json");
-			Object object = parser.parse(file);
-			JSONObject jsonobject = (JSONObject) object;
+			String fileReader = addressFile;
+			JSONObject jsonobject = file.readFile(fileReader);
 			if (jsonobject.get(firstName) != null) {
 				JSONArray jsonarray = (JSONArray) jsonobject.get(firstName);
 				JSONObject json = (JSONObject) jsonarray.get(0);
 				json.forEach((key, value) -> {
 					if (!key.equals(firstName)) {
 						System.out.println(key + "\t\t" + value);
-						System.out.println("enter 1 to edit the details or press any key");
-						int num = scanner.nextInt();
+						int num = 0;
+						try {
+							System.out.println("enter 1 to edit the details or press any key");
+							num = scanner.nextInt();
+						} catch (Exception e) {
+							System.out.println("enter digits only..");
+						}
 						if (num == 1) {
 							System.out.println("enter the details :");
 							if (key.equals("zipCode") || key.equals("phoneNumber")) {
@@ -88,9 +78,8 @@ public class AddressBookDAOIpml implements AddressDAO {
 				});
 				System.out.println("after change :");
 				json.forEach((key, value) -> System.out.println(key + "\t\t" + value));
-				FileWriter filewriter = new FileWriter("/home/bridgelabsz/Desktop/address.json");
-				filewriter.write(json.toJSONString());
-				filewriter.close();
+				String filewriter = addressFile;
+				file.writeFile(filewriter, json);
 			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -98,17 +87,15 @@ public class AddressBookDAOIpml implements AddressDAO {
 		}
 
 	}
-
 	/*
 	 * function to implement save addressbook
 	 */
 	@Override
 	public void saveAddressBook() throws IOException, ParseException {
-		JSONParser parser = new JSONParser();
+
 		try {
-			FileReader file = new FileReader("/home/bridgelabsz/Desktop/address.json");
-			Object object = parser.parse(file);
-			JSONObject jsonobject = (JSONObject) object;
+			String fileReader = addressFile;
+			JSONObject jsonobject = file.readFile(fileReader);
 			System.out.println("enter the name of address book :");
 			String name = scanner.next();
 			FileWriter filewriter = new FileWriter("/home/bridgelabsz/Desktop/" + name + ".json");
@@ -118,20 +105,16 @@ public class AddressBookDAOIpml implements AddressDAO {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
-
 	/*
-	 * function to implement write person 
+	 * function to implement write person
 	 */
 	@SuppressWarnings("unchecked")
 	@Override
 	public void writeDetails(String firstName, String lastName, String address, String city, String state, int zipCode,
 			long phoneNumber) throws IOException, ParseException {
-		JSONParser parser = new JSONParser();
-		FileReader file = new FileReader("/home/bridgelabsz/Desktop/address.json");
-		Object object = parser.parse(file);
-		JSONObject jsonobject = (JSONObject) object;
+		String fileReader = addressFile;
+		JSONObject jsonobject = file.readFile(fileReader);
 		JSONArray jsonarray = new JSONArray();
 		JSONObject jsonobj = new JSONObject();
 		jsonobj.put("firstName", firstName);
@@ -144,35 +127,23 @@ public class AddressBookDAOIpml implements AddressDAO {
 		jsonarray.add(jsonobj);
 		System.out.println(jsonarray);
 		jsonobject.put(firstName, jsonarray);
-		FileWriter filewriter = new FileWriter("/home/bridgelabsz/Desktop/address.json");
-		filewriter.write(jsonobject.toJSONString());
-		filewriter.close();
+		String filewriter = addressFile;
+		file.writeFile(filewriter, jsonobject);
 	}
-
 	/*
 	 * function to implement search person
 	 */
-	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
 	public void searchEntries(String firstname) throws IOException, ParseException {
-		JSONParser parser = new JSONParser();
 		try {
-			FileReader file = new FileReader("/home/bridgelabsz/Desktop/address.json");
-			Object object = parser.parse(file);
-			JSONObject jsonobject = (JSONObject) object;
+			String fileReader = addressFile;
+			JSONObject jsonobject = file.readFile(fileReader);
 			JSONArray jsonarray = (JSONArray) jsonobject.get(firstname);
-			if (jsonarray != null) {
-				Iterator iterator = jsonarray.iterator();
-				while (iterator.hasNext()) {
-					JSONObject json = (JSONObject) iterator.next();
-					json.forEach((key, value) -> System.out.println(key + "\t\t" + value));
-				}
-			}
+			System.out.println(jsonarray);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
 
 }
