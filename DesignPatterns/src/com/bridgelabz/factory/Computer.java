920cfcf6cc704da5484d2c8d6901e41ff7016e96
kishorereddy0909 <kishorereddy996@gmail.com>
2020-01-23 15:22:39 +0530

    [kishorereddy] Refractor. Comments added in Computer.java in com.bridgelabz.factory

--- DesignPatterns/src/com/bridgelabz/factory/Computer.java
+++ DesignPatterns/src/com/bridgelabz/factory/Computer.java
@@ -1,6 +1,13 @@
+/*purpose:declare interface with abstract methods
+ * @author kishorereddy
+ * @version 1.0
+ * @since 22/01/2020
+ * @file Computer.java
+*/
 package com.bridgelabz.factory;
 
 public abstract class Computer {
+	//abstract methods
 	public abstract String getRam();
 	public abstract String getHdd();
 	public abstract String getCpu();
