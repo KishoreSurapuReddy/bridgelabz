015c566ac3ead1e68c266dfbde2137841b9bc6cc
kishorereddy0909 <kishorereddy996@gmail.com>
2020-01-23 15:18:22 +0530

    [kishorereddy] Refractor. comments added in Iphone.java in com.bridgelabz.facadepattern

--- DesignPatterns/src/com/bridgeabz/facadepattern/IPhone.java
+++ DesignPatterns/src/com/bridgeabz/facadepattern/IPhone.java
@@ -1,3 +1,9 @@
+/*purpose:IPhone class implementing Phone interface 
+ * @author kishorereddy
+ * @version 1.0
+ * @since 22/01/2020
+ * @file IPhone.java
+*/
 package com.bridgeabz.facadepattern;
 
 public class IPhone implements Phone {
