fcdda3c64537b542c396bc74b37ef7d13dd0c4b6
kishorereddy0909 <kishorereddy996@gmail.com>
2020-01-23 15:27:29 +0530

    [kishorereddy] Refractor. comments added in ProtoType.java in com.bridgelabz.prototype

--- DesignPatterns/src/com/bridgelabz/prototype/ProtoType.java
+++ DesignPatterns/src/com/bridgelabz/prototype/ProtoType.java
@@ -1,3 +1,10 @@
+/*purpose:Prototype pattern will be used to copy the original object to 
+ * other object 
+ * @author kishorereddy
+ * @version 1.0
+ * @since 22/01/2020
+ * @file ProtoType.java
+*/
 package com.bridgelabz.prototype;
 
 import java.util.ArrayList;
@@ -7,7 +14,7 @@ public class ProtoType {
 	public static void main(String[] args) {
 		StudentClonable student = new StudentClonable();
 		student.loadData();
-		
+		//coping object to new object
 		ArrayList<Student> list1 = student.clone();
 		list1.remove(1);
 		System.out.println(student.getList());
