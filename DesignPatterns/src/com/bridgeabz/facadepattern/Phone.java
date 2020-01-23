0822229eeae50a6ce7dfdbafb123befadfb7f233
kishorereddy0909 <kishorereddy996@gmail.com>
2020-01-23 15:21:08 +0530

    [kishorereddy] Refractor. comments added in Phone.java in com.bridgelabz.facadepattern

--- DesignPatterns/src/com/bridgeabz/facadepattern/Phone.java
+++ DesignPatterns/src/com/bridgeabz/facadepattern/Phone.java
@@ -1,7 +1,13 @@
+/*purpose:declare interface 
+ * @author kishorereddy
+ * @version 1.0
+ * @since 22/01/2020
+ * @file Phone.java
+*/
 package com.bridgeabz.facadepattern;
 
 public interface Phone {
-	
+	//abstract method
 	public String mobile();
 
 }
