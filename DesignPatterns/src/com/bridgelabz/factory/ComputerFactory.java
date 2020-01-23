3e0f99433af2309fb7a377c4fab17d4cc0e8ec62
kishorereddy0909 <kishorereddy996@gmail.com>
2020-01-23 15:23:29 +0530

    [kishorereddy] Refractor. Comments added in ComputerFactory.java in com.bridgelabz.factory

--- DesignPatterns/src/com/bridgelabz/factory/ComputerFactory.java
+++ DesignPatterns/src/com/bridgelabz/factory/ComputerFactory.java
@@ -1,7 +1,13 @@
+/*purpose:FactoryPattern to interact with interfaces according to user requirement
+ * @author kishorereddy
+ * @version 1.0
+ * @since 22/01/2020
+ * @file ComputerFactory.java
+*/
 package com.bridgelabz.factory;
 
 public class ComputerFactory {
-	
+	//by user type it will instantiate
 	public static Computer getComputer(String type , String ram , String hdd , String cpu){
 		if("PC".equalsIgnoreCase(type)){
 			return new PC(ram, hdd, cpu);
