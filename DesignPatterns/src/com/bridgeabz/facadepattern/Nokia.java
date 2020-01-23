20b583c45500a5b44e6a58b26092fc4546fb129a
kishorereddy0909 <kishorereddy996@gmail.com>
2020-01-23 15:20:20 +0530

    [kishorereddy] Refractor. comments added in Nokia.java in com.bridgelabz.facadepattern

--- DesignPatterns/src/com/bridgeabz/facadepattern/Nokia.java
+++ DesignPatterns/src/com/bridgeabz/facadepattern/Nokia.java
@@ -1,3 +1,9 @@
+/*purpose:Nokia implementing abstract method  
+ * @author kishorereddy
+ * @version 1.0
+ * @since 22/01/2020
+ * @file Nokia.java
+*/
 package com.bridgeabz.facadepattern;
 
 public class Nokia implements Phone {
